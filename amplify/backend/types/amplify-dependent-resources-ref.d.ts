1cc6e55b308380139ca1bf90261ba5ae9481c295
Ubuntu <ubuntu@ip-10-0-0-4.ec2.internal>
2022-02-03 23:59:44 +0000

    Initial Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "postagramcd0069c1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "postagram": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "postagram": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
